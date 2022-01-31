af08d8dc1c3d563402826eee5c45e538ab3a8d1a
SergeyS85 <surzhiks.85@mail.ru>
2022-01-31 12:05:44 +0300

    create mediaquery and fix js "change show bg"

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,36 +1,47 @@
-(function(){
-  let i = 0;
-  function animationBg(){
-    
-    var background = document.querySelectorAll(".bg-container") 
-    setInterval(function(){    
-      i++;
-      if(i == background.length){
-        i = 0;
-        for(var j = 0; j < background.length;j++){
-          background[j].classList.remove('show-bg');
-        };      
-      }    
-      background[i].classList.add('show-bg')
-      if(background[i-1].classList.contains('show-bg')){
-        background[i-1].classList.remove('show-bg');
-      }
-    },8000)  
-  }
-  var resume       = document.querySelector('.resume');
-  var resume_block = document.querySelector('.resume_block')
-  function showResume(){ 
-    resume.addEventListener('click', function(){
-      resume_block.style.display = 'block'
-    })
-  }
-  function closeResume(){
-    var btnClose = document.querySelector('.close_resume');
-    btnClose.onclick = function(){
-      resume_block.style.display = null;
+(function() {
+
+
+    // //////////// Главная ///////////////
+    let background = document.querySelectorAll(".bg-container")
+    let i = 0;
+
+    function increment(callbackAnimBg) {
+        setInterval(() => {
+            if (i == background.length-1) {
+                i = 0;
+            }
+            else{
+              ++i
+            } 
+            callbackAnimBg()           
+        }, 4000)
     }
-  }
-  animationBg();
-  showResume();
-  closeResume();
+
+    function animationBg() {
+        for (let j = 0; j < background.length; ++j) {
+            background[j].classList.remove('show-bg');
+        };
+        background[i].classList.add('show-bg')
+    }
+
+    // ///////////// Резюме /////////////////
+    let resume = document.querySelector('.resume');
+    let resume_block = document.querySelector('.resume_block')
+
+    function showResume() {
+        resume.addEventListener('click', function() {
+            resume_block.style.display = 'block'
+        })
+    }
+
+    function closeResume() {
+        let btnClose = document.querySelector('.close_resume');
+        btnClose.onclick = function() {
+            resume_block.style.display = 'none';
+        }
+    }
+    increment(animationBg)
+    // animationBg();
+    showResume();
+    closeResume();
 })()
